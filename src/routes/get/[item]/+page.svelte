2a1ff6f70925188132cde7b86f659d5a26348744
JoJoJux aka. J0J0HA <info@jojojux.de>
2024-10-22 22:31:32 +0200

    Implement get-page

--- src/routes/get/[item]/+page.svelte
+++ src/routes/get/[item]/+page.svelte
@@ -6,36 +6,69 @@
 
     import { page } from "$app/stores";
     import FurnaceRecipe from "../../FurnaceRecipe.svelte";
+    import { db } from "$lib/db";
+    import { ItemStack } from "$lib/items";
+    import { liveQuery } from "dexie";
 
     import Body from "../../Body.svelte";
+    import ItemStackDisplay from "../../ItemStackDisplay.svelte";
 
-    $: filtered = getWaysToGet($page.params.item);
-    $: itemStack = getItemStack($page.params.item);
+    // $: filtered = getWaysToGet($page.params.item);
+    const item = liveQuery(
+        () => db.items.where({ fullId: $page.params.item }).first(),
+        { initialValue: null },
+    );
+    const block = liveQuery(
+        () => db.blockstates.where({ fullId: $page.params.item }).first(),
+        { initialValue: null },
+    );
+    $: itemStack =
+        $item || $block
+            ? new ItemStack($item || $block, 1, {})
+            : new ItemStack(null);
+
+    const craftingRecipes = liveQuery(() =>
+        db.craftingRecipes
+            .where("result.fullId")
+            .equals($page.params.item)
+            .toArray(),
+    );
+
+    const furnaceRecipes = liveQuery(
+        () =>
+            db.furnaceRecipes
+                .where("result.fullId")
+                .equals($page.params.item)
+                .toArray(),
+        { initialValue: [] },
+    );
 </script>
 
 <svelte:head>
-    <title>How to make {itemStack.getName()} - CR Recipes</title>
+    <title>How to make {itemStack?.name} - CR Recipes</title>
 </svelte:head>
 
 <Header />
-
+<a href="/">Back to item list</a>
 <Body>
-    <a href="/">Back to item list</a>
+    {#if !itemStack}
+        <p>Loading...</p>
+    {:else}
+        <h2>How to get</h2>
+        <ItemStackDetailDisplay {itemStack} />
 
-    <h2>How to get</h2>
-    <ItemStackDetailDisplay {itemStack} />
-
-    <div class="center">
-        {#each filtered.furnace as recipe}
-            <FurnaceRecipe {recipe} />
-        {/each}
-        {#each filtered.crafting as recipe}
-            <CraftingRecipe {recipe} />
-        {/each}
-        {#if filtered.noUse}
-            <p>{$page.params.item} has no recipes.</p>
-        {/if}
-    </div>
+        <div class="center">
+            {#each $furnaceRecipes || [] as recipe (recipe.id)}
+                <FurnaceRecipe {recipe} />
+            {/each}
+            {#each $craftingRecipes || [] as recipe (recipe.id)}
+                <CraftingRecipe {recipe} />
+            {/each}
+            {#if !$craftingRecipes?.length && !$furnaceRecipes?.length}
+                <p>{$page.params.item} has no recipes.</p>
+            {/if}
+        </div>
+    {/if}
 </Body>
 
 <style>
