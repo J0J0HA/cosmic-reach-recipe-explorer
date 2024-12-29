e5765c53c03f063eb8938795b990aff58af4e734
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2024-12-29 23:22:28 +0100

    fix error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,12 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
 <svelte:head>
-    <title>%sveltekit.status% - CR Recipes</title>
+    <title>{$page.status} - CR Recipes</title>
 </svelte:head>
 
-<div>
+<div style:margin-top="20px" style:margin-left="30px">
     <h1>Ooops...</h1>
-    <p>%sveltekit.status% - %sveltekit.error.message%</p>
+    <p>{$page.status} - {$page.error.message}</p>
 </div>
