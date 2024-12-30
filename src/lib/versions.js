c858d8076e376ba4aba259c9f2a086ba0175d4cb
Abhinav <itsabhinav5383@gmail.com>
2024-12-30 12:32:21 +0530

    - use fetch instead of axios - use a lighter color for links in dark mode - fix using fetch data without parsing it to json

--- src/lib/versions.js
+++ src/lib/versions.js
@@ -1,24 +1,39 @@
-import axios from "axios";
 import { getLoader, getZipFiles } from "./importer";
 import { store } from "./serializer";
 import { crVersion } from "./stores";
 import { setURLParam } from "./urlset";
 
 export async function getVersionList() {
-    const response = await axios.get("https://raw.githubusercontent.com/CRModders/CosmicArchive/main/versions.json", { responseType: "json" });
-    return response.data.versions.filter(version => !!version.client);
+    const res = await fetch("https://raw.githubusercontent.com/CRModders/CosmicArchive/main/versions.json");
+    const data = await res.json();
+    return data.versions.filter((version) => !!version.client);
 }
 
 export async function downloadVersion(version, stateCallback) {
-    const response = await axios.get(/*"/Cosmic Reach-0.3.1.jar" ||*/ version.client.url, {
-        responseType: "blob",
-        onDownloadProgress: (progressEvent) => stateCallback?.("download", progressEvent.loaded, progressEvent.total)
-    });
-    return response.data;
+    const res = await fetch(version.client.url);
+    const chunks = [];
+    const reader = res.body.getReader();
+
+    let downloadProgress = 0;
+    let totalSize = Number.parseInt(res.headers.get("Content-Length"));
+    if (Number.isNaN(totalSize) || totalSize <= 0) totalSize = version.client.size;
+
+    // Collect downloaded chunks and update the progress bar
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunks.push(value);
+        downloadProgress += value.length;
+
+        stateCallback?.("download", downloadProgress, totalSize);
+    }
+
+    const blob = new Blob(chunks);
+    return blob;
 }
 
 export async function setVersion(version, stateCallback) {
-    stateCallback?.("init")
+    stateCallback?.("init");
 
     store("loadedVersion", null);
     crVersion.set(null);
@@ -26,7 +41,9 @@ export async function setVersion(version, stateCallback) {
     await loader.activate();
 
     if (!loader) {
-        alert("No loader found for version " + version.id + "! This version is either invalid/unknown or not yet supported.\n\nTarget is to support all versions, so you might succeed by coming back later.");
+        alert(
+            `No loader found for version ${version.id}! This version is either invalid/unknown or not yet supported.\n\nTarget is to support all versions, so you might succeed by coming back later.`,
+        );
         return;
     }
 
@@ -36,11 +53,11 @@ export async function setVersion(version, stateCallback) {
 
     const files = await getZipFiles(versionJar);
 
-    stateCallback?.("parse")
+    stateCallback?.("parse");
 
     await loader.loadFiles("jar", files);
     crVersion.set(version.id);
     store("loadedVersion", version.id);
     setURLParam("version", version.id);
-    stateCallback?.("idle")
+    stateCallback?.("idle");
 }
